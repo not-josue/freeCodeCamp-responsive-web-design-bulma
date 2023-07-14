685375342ea9d65d016266a5dbb07bb950e9584b
not-josue <129870578+not-josue@users.noreply.github.com>
2023-07-14 06:29:37 -0400

    Add files via upload

--- product-landing-page/javascript/index.js
+++ product-landing-page/javascript/index.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-    burger();
+document.addEventListener("DOMContentLoaded", () => {
+  burger();
 });
 
 function burger() {
-    const burger = document.querySelector('.navbar-burger');
-    const navlinks = document.querySelector('.navbar-menu');
-    
-    burger.onclick = () => {
-        burger.classList.toggle('is-active');
-        navlinks.classList.toggle('is-active');
-    }
+  const burger = document.querySelector(".navbar-burger");
+  const navlinks = document.querySelector(".navbar-menu");
+
+  burger.onclick = () => {
+    burger.classList.toggle("is-active");
+    navlinks.classList.toggle("is-active");
+  };
 }
